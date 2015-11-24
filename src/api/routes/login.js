ff2ab28f9453594849afd26616eb24e43504a2c0
Kenrick Halff <khalff@jouwomgeving.nl>
2015-11-24 16:57:04 +0100

    empty out old version

--- src/api/routes/login.js
+++
@@ -1,8 +0,0 @@
-export default function login(req) {
-  const user = {
-    username: req.body.username,
-    password: req.body.password
-  };
-  req.session.user = user;
-  return Promise.resolve(user);
-}
